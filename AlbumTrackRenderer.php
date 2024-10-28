6a8fdca6c57641d6ab3f0ce18292e4f1e9b0111b
Packingdustry <alois.mc@hotmail.com>
2024-10-28 17:01:38 +0100

    td4.3 ajoute la classe ALbumTrackRenderer avec la méthode render()

---
+++ AlbumTrackRenderer.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+require_once "AlbumTrack.php";
+
+class AlbumTrackRenderer {
+    private AlbumTrack $albumTrack;
+
+    public function __construct(AlbumTrack $albumTrack) {
+        $this->albumTrack = $albumTrack;
+    }
+
+    public function render(int $selector): string {
+        if ($selector == 1) {
+            return $this->renderCompact();
+        } else {
+            return $this->renderLong();
+        }
+    }
+
+    private function renderCompact(): string {
+        return "<h3>{$this->albumTrack->titre}</h3><p>{$this->albumTrack->artiste}</p>";
+    }
+
+    private function renderLong(): string {
+        $res = "
+            <h3>{$this->albumTrack->titre}</h3>
+            <p>{$this->albumTrack->artiste}</p>
+            <p>{$this->albumTrack->album}</p>
+            <p>{$this->albumTrack->genre}</p>
+            <p>{$this->albumTrack->nomFichier}</p>
+            <p>{$this->albumTrack->annee}</p>
+            <p>{$this->albumTrack->numeroPiste}</p>
+            <p>{$this->albumTrack->duree}</p>
+        ";
+        return $res;
+    }
+}
+?>
